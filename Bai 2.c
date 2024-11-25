73b22c67dddd6194f9665299e034dc46d3ead515
LeeAnhs <138470988+LeeAnhs@users.noreply.github.com>
2024-11-25 17:13:53 +0700

    Bài 2

---
+++ Bai 2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main () {
+	int correctans=25;
+	int number;
+	while(number != correctans) {
+		printf("Nhap vao so nguyen bat ky: ");
+		scanf("%d", &number);
+			
+		}	
+		if(number!=correctans){
+		printf("So bn nhap khong dung\n");
+		
+		} else {
+			printf ("Dung roi\n");
+			
+		}
+		return 0;
+	}
+   
+
